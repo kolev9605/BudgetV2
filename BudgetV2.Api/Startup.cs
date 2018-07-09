81553c05a6430a0875d8b43021d194c7a690deb9
Georgi Kolev <georgi.kolev@mentormate.com>
2018-07-09 15:40:05 +0300

    package references fixed

--- BudgetV2.Api/Startup.cs
+++ BudgetV2.Api/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
@@ -85,23 +84,23 @@ namespace BudgetV2.Api
                 app.UseHsts();
             }
 
-            app.UseExceptionHandler(
-                builder =>
-                {
-                    builder.Run(
-                        async context =>
-                        {
-                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            //app.UseExceptionHandler(
+            //    builder =>
+            //    {
+            //        builder.Run(
+            //            async context =>
+            //            {
+            //                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            //                context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
 
-                            var error = context.Features.Get<IExceptionHandlerFeature>();
-                            if (error != null)
-                            {
-                                context.Response.AddApplicationError(error.Error.Message);
-                                await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
-                            }
-                        });
-                });
+            //                var error = context.Features.Get<IExceptionHandlerFeature>();
+            //                if (error != null)
+            //                {
+            //                    context.Response.AddApplicationError(error.Error.Message);
+            //                    await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
+            //                }
+            //            });
+            //    });
 
             app.UseAuthentication();
             app.UseHttpsRedirection();
