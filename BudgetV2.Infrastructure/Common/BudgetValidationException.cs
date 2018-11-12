24fe6e128e868858694affd1f9cd2ba455b1eb53
Georgi Kolev <georgi.kolev@mentormate.com>
2018-11-12 19:42:43 +0200

    BE: Add BudgetValidationException

---
+++ BudgetV2.Infrastructure/Common/BudgetValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BudgetV2.Infrastructure.Common
+{
+    [Serializable]
+    public class BudgetValidationException : Exception
+    {
+        public BudgetValidationException(string message) : base(message)
+        {
+        }
+
+        public BudgetValidationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
